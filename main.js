ae7696309fd70e3979f67ad310eee23c0c5d00fe
Priyanshi-Dhawan <78725801+Priyanshi-Dhawan@users.noreply.github.com>
2022-10-08 11:56:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+function modelLOaded() {
+    console.log('posenet is Initialized')
+}
+//for moustache//
+function gotposes(results)
+{
+    if (results.legnth>0)
+    console.log(results);
+    console.log("lips x =" = results[0].pose.lips,x);
+    console.log("lips y ="+ results[0].poose.lips.y);
+}
+
+
+function draw(){
+    image(video,0,0,300,300)
+}
+function take_snapshot(){
+    SVGAElement('myFilterImage.png')
+}
+
+lips X=0;
+lips Y=0;
+
+function gotPoses (results){
+    if results[0].pose.noes.y
+    if results[0].pose.lips.y
+    console.log("lips y="+lips y)
+    console.log("lips x="+lips x)
+
+}
+function draw()
+Image(0,0,300,300);
+circle(lips X,lips Y,20)
+
+function preload(){
+    clown lips= load image (https:https://i.postimg.cc/280gWcqF/lips.png)
+
+}
+
+function draw()
+image(video,0,0,300,300)
+fill(77, 38, 0)
+stroke77, 38, 0)
+circle(lips X,lips Y,30,30)
+
+//for lips//
+function gotposes(results)
+{
+    if (results.legnth>0)
+    console.log(results);
+    console.log("lips x =" = results[0].pose.lips,x);
+    console.log("lips y ="+ results[0].poose.lips.y);
+}
+
+
+function draw(){
+    image(video,0,0,300,300)
+}
+
+
+function gotPoses (results){
+    if results[0].pose.lips.y
+    if results[0].pose.lips.y
+    console.log("lips y="+lips y)
+    console.log("lips x="+lips x)
+
+}
+function draw()
+Image(0,0,300,300);
+circle(lips X,lips Y,20)
+
+function preload(){
+    clown lips= load image (https:https://i.postimg.cc/RVpbhW8g/mouste.png)
+
+}
+
+function draw()
+image(video,0,0,300,300)
+fill(255, 77, 77)
+stroke(255, 77, 77)
+circle(lips X,lips Y,30,30)
